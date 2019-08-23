6ee73af02aef4728360a68101b9040d8fd0f1642
Nole <universe_fly@icloud.com>
2019-08-23 17:08:24 +0800

    Initial Commit

---
+++ SubwayTransferSystem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SubwayTransferSystem
+//
+//  Created by 魏宇翔 on 2019/8/23.
+//  Copyright © 2019 Tongji University. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
